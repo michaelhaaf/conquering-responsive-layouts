d9387fdd95bcc64edfa5330fff4f4c7036b415e0
michaelhaaf <michael.haaf@gmail.com>
2023-04-29 19:17:45 -0400

    final challenge solution draft

--- challenge10/main.js
+++ challenge10/main.js
@@ -1,7 +1,24 @@
-const navToggle = document.querySelector('.page-nav-toggle');
+const baselinerToggle = document.querySelector('.logo');
+const navToggle = document.querySelector('.toggle');
 const nav = document.querySelector('.page-nav');
+
 
 navToggle.addEventListener('click', () => {
     nav.classList.toggle('page-nav--visible');
-    console.log(nav.classList);
-})
+});
+
+// https://keyes.ie/things/baseliner/  
+// This tool creates a bunch of divs, which can cause vertical overflow.
+// I had to add some jank to remove those elems. TODO refactor for brevity/clarity
+baselinerToggle.addEventListener('click', () => {
+  let baselinerElem = document.querySelector('#overlay-it');
+  let baselinerOverlay = document.querySelector('#baseline-overlay');
+  if (baselinerElem) {
+    document.body.removeChild(baselinerElem.parentElement)
+    if (baselinerOverlay) {
+      document.body.removeChild(baselinerOverlay) 
+    }
+  } else {
+    baseliner = new Baseliner({gridHeight: 32});
+  } 
+});
